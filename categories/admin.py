adca40da4557c411e31c993e4dba31dd1d06fcb6
Luisf66 <Luisf311220@gmail.com>
2026-02-27 08:16:24 -0300

    feat: CRUD de category #1

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+admin.site.register(models.Categories, CategoriesAdmin)
