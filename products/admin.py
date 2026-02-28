a859cfa6d7d18b35e7845d4d8c1d2391766a8cca
Luisf66 <Luisf311220@gmail.com>
2026-02-28 10:51:20 -0300

    fix: Correção no admin de Produtos #9

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,7 @@ from . import models
 # Register your models here.
 
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'brand', 'price', 'created_at', 'updated_at')
-    search_fields = ('name', 'category', 'brand')
+    list_display = ('title', 'category', 'brand', 'cost_price', 'selling_price', 'quantity', 'created_at', 'updated_at')
+    search_fields = ('title', 'category', 'brand')
 
 admin.site.register(models.Product, ProductsAdmin)
