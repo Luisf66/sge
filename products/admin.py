ef16441d18f0cfad191261f75809854a3dd356b0
Luisf66 <Luisf311220@gmail.com>
2026-02-28 10:20:39 -0300

    feat: Admin + Views para Produtos #9

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'price', 'created_at', 'updated_at')
+    search_fields = ('name', 'category', 'brand')
+
+admin.site.register(models.Product, ProductsAdmin)
