381eae5ac197687562cdeb88c7555e6d25371aac
Luisf66 <Luisf311220@gmail.com>
2026-03-02 13:32:31 -0300

    feat: Adição base de métricas #15

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name='home'),
     
     path('', include('brand.urls')),
     path('', include('categories.urls')),
