5b0380f25764b27b9964e758c4cd15b99da92b09
Luisf66 <Luisf311220@gmail.com>
2026-03-07 20:47:53 -0300

    feat: CRUD de Brand via API #23

--- brand/urls.py
+++ brand/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('brand/<int:pk>/detail/', views.BrandDetailView.as_view(), name='brand_detail'),
     path('brand/<int:pk>/update/', views.BrandUpdateView.as_view(), name='brand_update'),
     path('brand/<int:pk>/delete/', views.BrandDeleteView.as_view(), name='brand_delete'),
+
+    path('api/v1/brands/', views.BrandListCreateApiView.as_view(), name='brand-list-create-api-view'),
+    path('api/v1/brands/<int:pk>/', views.BrandRetrieveUpdateDestroyApiView.as_view(), name='brand-detail-api-view'),
 ]
